a46290ad425927eaf1e2a26bb054a11181707fe5
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-08-28 21:01:27 +0200

    Fixed unit tests.

--- src/test/java/pl/winiarczyk/lesson4/homework/AgeVerifyTest.java
+++ src/test/java/pl/winiarczyk/lesson4/homework/AgeVerifyTest.java
@@ -14,29 +14,12 @@ class AgeVerifyTest {
 
         Assertions.assertTrue(isOver18);
     }
-
     @Test
-    void shouldCheckIfAgeIsOver() {
-        int testMatureAge2 = 59;
+    void shouldCheckIfAgeIsUnder18() {
+        int testMatureAge = 14;
 
-        boolean isOver18 = AgeVerify.isMature(testMatureAge2);
+        boolean isOver18 = AgeVerify.isMature(testMatureAge);
 
-        Assertions.assertTrue(isOver18);
-    }
-    @Test
-    void shouldCheckAge() {
-        int testNotMatureAge = 11;
-
-        boolean isOver = AgeVerify.isMature(testNotMatureAge);
-
-        Assertions.assertFalse(isOver);
-    }
-    @Test
-    void shouldCheckIfAgeIsMature() {
-        int testNotMatureAge = 3;
-
-        boolean isOver = AgeVerify.isMature(testNotMatureAge);
-
-        Assertions.assertFalse(isOver);
+        Assertions.assertFalse(isOver18);
     }
 }

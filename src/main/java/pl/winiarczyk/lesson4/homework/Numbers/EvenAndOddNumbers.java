216e3a9656de3f9d28d840156c66f73e493e739a
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-08-28 20:57:20 +0200

    Fixed method.

--- src/main/java/pl/winiarczyk/lesson4/homework/Numbers/EvenAndOddNumbers.java
+++ src/main/java/pl/winiarczyk/lesson4/homework/Numbers/EvenAndOddNumbers.java
@@ -3,16 +3,10 @@ package pl.winiarczyk.lesson4.homework.Numbers;
 public class EvenAndOddNumbers {
 
     public static boolean isNumberOddOrEven(int checkNumber) {
-        if (true) {
-            return checkNumber % 2 == 0;
-        }
-        if (false) {
-            return checkNumber % 2 != 0;
-        }
-        return true;
+        return checkNumber % 2 == 0;
     }
 
     public static void main(String[] args) {
-        System.out.println(isNumberOddOrEven(4989));
+        System.out.println(isNumberOddOrEven(498));
     }
 }

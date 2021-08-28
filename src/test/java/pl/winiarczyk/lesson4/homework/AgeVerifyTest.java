324fcac2481cfa5516d1bab3edce13002848398d
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-08-28 11:33:41 +0200

    Add more unit tests

--- src/test/java/pl/winiarczyk/lesson4/homework/AgeVerifyTest.java
+++ src/test/java/pl/winiarczyk/lesson4/homework/AgeVerifyTest.java
@@ -14,4 +14,29 @@ class AgeVerifyTest {
 
         Assertions.assertTrue(isOver18);
     }
+
+    @Test
+    void shouldCheckIfAgeIsOver() {
+        int testMatureAge2 = 59;
+
+        boolean isOver18 = AgeVerify.isMature(testMatureAge2);
+
+        Assertions.assertTrue(isOver18);
+    }
+    @Test
+    void shouldCheckAge() {
+        int testNotMatureAge = 11;
+
+        boolean isOver = AgeVerify.isMature(testNotMatureAge);
+
+        Assertions.assertFalse(isOver);
+    }
+    @Test
+    void shouldCheckIfAgeIsMature() {
+        int testNotMatureAge = 3;
+
+        boolean isOver = AgeVerify.isMature(testNotMatureAge);
+
+        Assertions.assertFalse(isOver);
+    }
 }

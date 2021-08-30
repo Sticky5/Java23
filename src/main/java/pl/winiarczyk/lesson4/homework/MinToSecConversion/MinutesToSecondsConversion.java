a71f058a37c0deb78a3be08fdab1651bb4dbe620
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-08-30 14:52:27 +0200

    OOP1 Exercise - Conver minutes to seconds.

---
+++ src/main/java/pl/winiarczyk/lesson4/homework/MinToSecConversion/MinutesToSecondsConversion.java
@@ -0,0 +1,16 @@
+package pl.winiarczyk.lesson4.homework.MinToSecConversion;
+
+class MinutesToSecondsConversion {
+
+    double timeInMinutes;
+
+    public MinutesToSecondsConversion(double minutes) {
+        this.timeInMinutes = timeInMinutes;
+    }
+
+    public static double convertMinutesToSeconds(double timeInMinutes) {
+
+        double timeInSeconds;
+        return timeInSeconds = timeInMinutes * 60;
+    }
+}
---
+++ src/test/java/pl/winiarczyk/lesson4/homework/MinToSecConversion/MinutesToSecondsConversionTest.java
@@ -0,0 +1,27 @@
+package pl.winiarczyk.lesson4.homework.MinToSecConversion;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MinutesToSecondsConversionTest {
+
+    double timeInMinutes;
+
+    @Test
+    public void shouldConvert44MinutesToSeconds() {
+        timeInMinutes = 44;
+
+        double timeInSeconds = MinutesToSecondsConversion.convertMinutesToSeconds(timeInMinutes);
+
+        Assertions.assertEquals(timeInSeconds, 2640);
+    }
+
+    @Test
+    public void shouldConvert0MinutesToSeconds() {
+        timeInMinutes = 0;
+
+        double timeInSeconds = MinutesToSecondsConversion.convertMinutesToSeconds(timeInMinutes);
+
+        Assertions.assertEquals(timeInSeconds, 0);
+    }
+}

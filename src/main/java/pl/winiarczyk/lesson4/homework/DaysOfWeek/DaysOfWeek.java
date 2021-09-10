862177f9c7e973bfed2db4c7904fd6335f51eeb4
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-09-10 17:05:21 +0200

    Done person class.

--- src/main/java/pl/winiarczyk/lesson4/homework/DaysOfWeek/DaysOfWeek.java
+++
@@ -1,8 +0,0 @@
-package pl.winiarczyk.lesson4.homework.DaysOfWeek;
-
-public class DaysOfWeek {
-
-    public String getNameOfDay(NameOfDay dayName) {
-        return dayName.toString();
-    }
-}
---
+++ src/main/java/pl/winiarczyk/lesson4/homework/Person/Person.java
@@ -0,0 +1,47 @@
+package pl.winiarczyk.lesson4.homework.Person;
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private String bornCity;
+    private int inssuranceNumber;
+
+    public Person(String name, String surname, String bornCity, int inssuranceNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.bornCity = bornCity;
+        this.inssuranceNumber = inssuranceNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getBornCity() {
+        return bornCity;
+    }
+
+    public int getInssuranceNumber() {
+        return inssuranceNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return inssuranceNumber == person.inssuranceNumber && Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(bornCity, person.bornCity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, bornCity, inssuranceNumber);
+    }
+}

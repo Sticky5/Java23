19c4110f32d0c66341518016378c41ad00a20f8c
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-09-10 03:05:15 +0200

    Done FizzBuzz game.

---
+++ src/main/java/pl/winiarczyk/lesson4/homework/FizzBuzzGame/FizzBuzz.java
@@ -0,0 +1,16 @@
+package pl.winiarczyk.lesson4.homework.FizzBuzzGame;
+
+public class FizzBuzz {
+
+    public void fizzBuzz() {
+        for (int i = 1; i <= 100; i++)
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+    }
+}

e0846ec5bce6728da89e34d392cd1f8940b9d759
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-09-12 00:25:19 +0200

    Fixed data.txt file reader.

---
+++ src/main/java/pl/winiarczyk/lesson4/homework/TextFile/FileReader.java
@@ -0,0 +1,14 @@
+package pl.winiarczyk.lesson4.homework.TextFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public abstract class FileReader {
+
+    public static void main(String[] args) throws IOException {
+
+        String text = Files.readString(Path.of("C:\\Users\\Lenovo\\Java23\\data.txt"));
+        System.out.println(text);
+    }
+}

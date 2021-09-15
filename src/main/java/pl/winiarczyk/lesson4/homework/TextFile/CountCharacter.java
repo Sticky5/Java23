a9c3d4442d3d2adc4e8ad6d54293b081a6beceb2
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-09-15 22:10:03 +0200

    Fixed file path.

--- src/main/java/pl/winiarczyk/lesson4/homework/TextFile/CountCharacter.java
+++ src/main/java/pl/winiarczyk/lesson4/homework/TextFile/CountCharacter.java
@@ -5,14 +5,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class CountCharacter {
+
     public static void main(String[] args) throws IOException {
 
-        String text = Files.readString(Path.of("C:\\Users\\Lenovo\\Java23\\data.txt"));
+        String text = Files.readString(Path.of("data.txt"));
 
         int count = 0;
         for (int i = 0; i < text.length(); i++) {
             count++;
         }
+
         System.out.println("Number of characters in data.txt is: " + count);
     }
 }

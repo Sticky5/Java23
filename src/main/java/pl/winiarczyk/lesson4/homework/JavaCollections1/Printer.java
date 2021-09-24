6e353d0a1be4a69e9c7b5c45e78c664c009a3023
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-09-24 12:25:33 +0200

    Done exercise Java Collections 1.

---
+++ src/main/java/pl/winiarczyk/lesson4/homework/JavaCollections1/Printer.java
@@ -0,0 +1,32 @@
+package pl.winiarczyk.lesson4.homework.JavaCollections1;
+
+import java.util.*;
+
+public class Printer {
+    public static void printWithoutDuplicates(List<String> list) {
+        Set<String> set = new LinkedHashSet<>(list);
+        List<String> listWithoutDuplicates = new ArrayList<>(set);
+
+        System.out.println("List without duplicates: " + listWithoutDuplicates);
+    }
+
+    public static void printReversed(List<String> list) {
+        List<String> reversedList = new ArrayList<>(list);
+        Collections.reverse(reversedList);
+
+        System.out.println("List in reversed order: " + reversedList);
+    }
+
+    public static void printWithReplacedElements(String toReplace, String replacement, List<String> list) {
+        List<String> listWithReplacedElements = new ArrayList<>(list);
+        ListIterator<String> iterator = listWithReplacedElements.listIterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            if(next.equals(toReplace)) {
+                iterator.set(replacement);
+            }
+        }
+
+        System.out.println("List with element " + toReplace + " replaced to " + replacement + ": " + listWithReplacedElements);
+    }
+}

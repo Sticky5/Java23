6801c60ef5308a3b922bbf6bbb72c75b5b912745
Daniel Winiarczyk <dwiniarczyk1@gmail.com>
2021-10-07 11:41:17 +0200

    Done Java Collections 2 exercise 4.

---
+++ src/main/java/pl/winiarczyk/lesson4/homework/JavaCollections2_4/App.java
@@ -0,0 +1,9 @@
+package pl.winiarczyk.lesson4.homework.JavaCollections2_4;
+
+public class App {
+    public static void main(String[] args) {
+        Pair<Integer, String> pair = new Pair<>(1, "Daniel");
+        System.out.println(pair.getKey());
+        System.out.println(pair.getValue());
+    }
+}
